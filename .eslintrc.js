4196147646f4cf9418033722ac0bda81ba66f863
ashwin <ashwinkn@hotmail.com>
2019-03-13 14:45:59 -0500

    Fixed flow config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['plugin:react/recommended', 'eslint:recommended', 'plugin:jest/recommended', 'prettier'],
-    plugins: ['react', 'babel', 'jest'],
+    plugins: ['react', 'babel', 'jest', 'flowtype'],
     parser: 'babel-eslint',
     env: {
         browser: true,
@@ -19,7 +19,32 @@ module.exports = {
         'no-redeclare': 'warn',
         quotes: ['error', 'single', { avoidEscape: true }],
         semi: ['error', 'always'],
-        'generator-star-spacing': ['error', { before: true, after: false }]
+        'generator-star-spacing': ['error', { before: true, after: false }],
+        'flowtype/boolean-style': [2, 'boolean'],
+        'flowtype/define-flow-type': 1,
+        'flowtype/delimiter-dangle': [2, 'never'],
+        'flowtype/generic-spacing': [2, 'never'],
+        'flowtype/no-primitive-constructor-types': 2,
+        'flowtype/no-types-missing-file-annotation': 2,
+        'flowtype/no-weak-types': 2,
+        'flowtype/object-type-delimiter': [2, 'comma'],
+        'flowtype/require-parameter-type': 2,
+        'flowtype/require-return-type': [
+            2,
+            'always',
+            {
+                annotateUndefined: 'never'
+            }
+        ],
+        'flowtype/require-valid-file-annotation': 2,
+        'flowtype/semi': [2, 'always'],
+        'flowtype/space-after-type-colon': [2, 'always'],
+        'flowtype/space-before-generic-bracket': [2, 'never'],
+        'flowtype/space-before-type-colon': [2, 'never'],
+        'flowtype/type-id-match': [2, '^([A-Z][a-z0-9]+)+Type$'],
+        'flowtype/union-intersection-spacing': [2, 'always'],
+        'flowtype/use-flow-type': 1,
+        'flowtype/valid-syntax': 1
     },
     parserOptions: {
         ecmaFeatures: { jsx: true },
@@ -43,6 +68,9 @@ module.exports = {
             'babel-module': {
                 extensions: ['.jsx', '.js']
             }
+        },
+        flowtype: {
+            onlyFilesWithFlowAnnotation: true
         }
     }
 };
--- src/js/containers/RootContainer.jsx
+++ src/js/containers/RootContainer.jsx
@@ -1,5 +1,3 @@
-// @flow
-
 import React, { Component } from 'react';
 
 import { BrowserRouter as Router } from 'react-router-dom';

472d6e0909d8ba8477b4dbae0d5691f2415357d0
ashwin <ashwinkn@hotmail.com>
2019-03-19 15:46:21 -0500

    generated by     - gen-lib@1.1.2     - @kurlytail/gen-log@1.1.3     - @kurlytail/gen-react@1.1.3     - @kurlytail/gen-npm@1.1.1     - @kurlytail/gen-bootstrap4@1.1.1

--- src/js/containers/GraphContainer.jsx
+++ src/js/containers/GraphContainer.jsx
@@ -1,13 +1,8 @@
-import { ButtonComponent } from '../components';
 import React, { Component } from 'react';
 
 class GraphContainer extends Component {
     render() {
-        const Container = (
-            <div className={'container-fluid'}>
-                <ButtonComponent label="rebuild" />
-            </div>
-        );
+        const Container = <div className={'container-fluid'} />;
         return Container;
     }
 }
--- src/js/containers/RootContainer.jsx
+++ src/js/containers/RootContainer.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import GraphContainer from './GraphContainer';
+import { ButtonComponent } from '../components';
 import React, { Component } from 'react';
 
 class RootContainer extends Component {
     render() {
         const Container = (
             <div className={'container-fluid'}>
+                <ButtonComponent label="rebuild" />
+
                 <Route path={'*'} component={GraphContainer} />
             </div>
         );
